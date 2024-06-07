7b63f5c154d188ad40ab42103458fc1e9d6e28ba
DAVID <davidpintodev97@gmail.com>
2024-06-07 17:53:52 +0200

    Inicio modelo de clases, Usuario, Evento

---
+++ src/main/java/es/events/modelo/Evento.java
@@ -0,0 +1,13 @@
+package es.events.modelo;
+
+import java.io.Serializable;
+
+public class Evento implements Serializable, Comparable<Evento>{
+
+	@Override
+	public int compareTo(Evento o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/es/events/modelo/Usuario.java
@@ -0,0 +1,13 @@
+package es.events.modelo;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable, Comparable<Usuario> {
+
+	@Override
+	public int compareTo(Usuario o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
