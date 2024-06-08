fc8b82f72471fb3cf545ebe566132f8a27d1c1a4
DAVID <davidpintodev97@gmail.com>
2024-06-08 21:59:15 +0200

    Actualizacion Modelo de Clases java

--- src/main/java/es/events/modelo/Evento.java
+++ src/main/java/es/events/modelo/Evento.java
@@ -1,8 +1,16 @@
 package es.events.modelo;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Evento implements Serializable, Comparable<Evento>{
+	//ATRIBUTOS PRIVADOS DE CLASE:
+	private int idEvento;
+	private String nombre;
+	private String descripcion;
+	private String ubicacion;
+	private LocalDateTime fechaHora; //GUARDO EN ESTE TIPO DE DATO, YA QUE ES MODERNO (JAVA8 EN ADELANTE), Y NO NECESITO PRECISIÓN EN NANOSEGUNDOS
+	private Usuario usuario;
 
 	@Override
 	public int compareTo(Evento o) {
--- src/main/java/es/events/modelo/Usuario.java
+++ src/main/java/es/events/modelo/Usuario.java
@@ -1,9 +1,20 @@
 package es.events.modelo;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Usuario implements Serializable, Comparable<Usuario> {
-
+	//ATRIBUTOS PRIVADOS DE CLASE:
+	private int idUsuario;
+	private String nombre;
+	private String ape1;
+	private String ape2;
+	private String nickname;
+	private String correo;
+	private String password;
+	private LocalDateTime fechaRegistro; //GUARDO EN ESTE TIPO DE DATO, YA QUE ES MODERNO (JAVA8 EN ADELANTE), Y NO NECESITO PRECISIÓN EN NANOSEGUNDOS
+	
+	
 	@Override
 	public int compareTo(Usuario o) {
 		// TODO Auto-generated method stub
