d7751b02b25505ba79d73d3e09f8c29c8b6ccc80
DAVID <davidpintodev97@gmail.com>
2024-06-17 22:25:12 +0200

    Incluida depend.JUnit5 en pom.xml y creado TestConexionBBDD

--- src/test/java/es/events/persistencia/T0Conexion.java
+++ src/test/java/es/events/persistencia/T0Conexion.java
@@ -1,10 +1,47 @@
 package es.events.persistencia;
 
+
+import org.junit.jupiter.api.Test;
+
 import jakarta.persistence.EntityManager;
 
 public class T0Conexion {
-	//OBTENGO INSTANCIA DEL EMF:
-	EntityManager em = EMF.getInstance().createEntityManager();
+	//CREO EL METODO; NOTA: DESPLEGAR PARA VER QUÉ HACE EL MÉTODO
+	/**
+	 * Metodo para comprobar si se conecta bien a la BBDD
+	 * @author David
+	 * BLOQUE TRY-FINALLY: TRY, PARA VERIFICAR SI SE CONECTÓ O NO A LA BBDD; 
+	 * FINALLY, SE EJECUTA INDEPENDIOENTEMENTE DE SI HAY EXCEPCIÓN EN EL TRY; SIRVE PARA CERRAR EL em EN CUALQUIER CASO Y NO HAY FUGA DE RECURSOS
+	 */
+	@Test
+	public void comprobarConex(){
+		//OBTENGO INSTANMCIA DEL EMF:
+		EntityManager em = EMF.getInstance().createEntityManager();
+		
+		//BLOQUE TRY-FINALLY:
+		try {
+			if (em.isOpen()) {
+				System.out.println("Conexión efectiva a la BBDD");
+				
+			}else {
+				System.err.println("Error en la conexión a la BBDD");
+			}
+			
+		} finally {
+			//CERRAR EL EM:
+			if(em.isOpen())
+				em.close();
+		}
+			
+	}
+}
+	
+	
+	
+	
 	
 
-}
+	
+	
+
+
